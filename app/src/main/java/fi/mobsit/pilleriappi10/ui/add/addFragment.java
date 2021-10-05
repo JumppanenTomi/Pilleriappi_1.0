b8acbb7b6f62604c136ba6aa9feb77c0ab7f9090
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2021-10-05 14:14:39 +0300

    Backup.

--- app/src/main/java/fi/mobsit/pilleriappi10/ui/add/addFragment.java
+++ app/src/main/java/fi/mobsit/pilleriappi10/ui/add/addFragment.java
@@ -1,9 +1,12 @@
 package fi.mobsit.pilleriappi10.ui.add;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import fi.mobsit.pilleriappi10.R;
 import fi.mobsit.pilleriappi10.databinding.FragmentAddBinding;
 
 public class addFragment extends Fragment {
@@ -24,8 +28,24 @@ public class addFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.v("VIESTI", "ADD-sivu avattu");
+        super.onViewCreated(view, savedInstanceState);
+        EditText medicineNameTextbox = (EditText) view.findViewById(R.id.medicineNameTextbox);
+
+        Button addMedicineButton = (Button) view.findViewById(R.id.addMedicineButton);
+
+        addMedicineButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.v("VIESTI", "Lääke lisätty");
+            }
+        });
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        Log.v("VIESTI", "ADD-sivu suljettu");
     }
 }
