433daf952d373c412fad9bf2ec326c1768a7ebbe
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2021-10-07 18:24:17 +0300

    Edellisessä pushissa oli ongelmana että uudelleen käynnistäminen poisti vanhan datan. nyt asia on korjattu

--- app/src/main/java/fi/mobsit/pilleriappi10/ui/add/addFragment.java
+++ app/src/main/java/fi/mobsit/pilleriappi10/ui/add/addFragment.java
@@ -3,7 +3,6 @@ package fi.mobsit.pilleriappi10.ui.add;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,21 +17,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import fi.mobsit.pilleriappi10.R;
-import fi.mobsit.pilleriappi10.databinding.FragmentAddBinding;
+import fi.mobsit.pilleriappi10.databinding.FragmentAddBinding;//
 
 public class addFragment extends Fragment {
     private FragmentAddBinding binding;
-    Context context;
+    private String oldDataJson;
 
     public static final  String SHARED_PREFS = "pendingMedicineData";
-    public static  final String MEDICINE_NAME = "name";
-
-    private String medicineName;
-
+    public static  final String MEDICINE_ARRAY = "medicineArray";
 
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,25 +37,27 @@ public class addFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        ArrayList<medicine> medicines = new ArrayList<>();
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        oldDataJson = sharedPreferences.getString(MEDICINE_ARRAY, "");
+
+        ArrayList<medicine> medicines = new ArrayList<>();;
         super.onViewCreated(view, savedInstanceState);
-        Button addMedicineButton = (Button) view.findViewById(R.id.addMedicineButton);
+        Button addMedicineButton = view.findViewById(R.id.addMedicineButton);
 
         addMedicineButton.setOnClickListener(v -> {
-            EditText medicineNameTextbox = (EditText) view.findViewById(R.id.medicineNameTextbox);
-            DatePicker medicineTakingDateTextbox = (DatePicker) view.findViewById(R.id.medicineTakingDateTextbox);
-            TimePicker medicineTakingTimepicker = (TimePicker) view.findViewById(R.id.medicineTakingTimepicker);
+            EditText medicineNameTextbox = view.findViewById(R.id.medicineNameTextbox);
+            DatePicker medicineTakingDateTextbox = view.findViewById(R.id.medicineTakingDateTextbox);
+            TimePicker medicineTakingTimepicker = view.findViewById(R.id.medicineTakingTimepicker);
 
             medicine newMedicine = new medicine(medicineNameTextbox.getText().toString(), medicineTakingTimepicker.getCurrentMinute(), medicineTakingTimepicker.getCurrentHour(), medicineTakingDateTextbox.getDayOfMonth(), medicineTakingDateTextbox.getMonth() + 1, medicineTakingDateTextbox.getYear());
-            medicines.add(newMedicine);
-
-
-            SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            Gson gson = new Gson();
-            String json = gson.toJson(medicines);
-            editor.putString(MEDICINE_NAME, json);
-            editor.apply();
+            medicines.add(newMedicine);//Creating new object for added medicine
+            SharedPreferences.Editor editor = sharedPreferences.edit();//starting sharedPreferences editor
+            Gson gson = new Gson();//creating new object called gson
+            String json = oldDataJson;
+            json += gson.toJson(medicines);//telling to gson object to convert ArrayList called medicines to string variable called "json"
+            String trimmedJson = json.replaceAll("&quot", "");//trimming json file. Removing "&quot" from array. REMOVE IF NECESSARILY!!!!
+            editor.putString(MEDICINE_ARRAY, trimmedJson);//adding value of variable "json" to sharedPreferences editor queue
+            editor.apply();//applying changes to sharedPreferences from last editor line
 
             Toast.makeText(getActivity(),"Lääke lisätty",Toast.LENGTH_SHORT).show();
             //Navigation.findNavController(view).navigate(R.id.navigation_home);  //return user back to home-fragment after submitting new form
