ccf18b755b4c0c6451000480b3acb853c68469a6
Andreas <djdreviz@gmail.com>
2021-10-08 14:05:05 +0300

    Settints fragmentin UIn luomista

--- app/src/main/java/fi/mobsit/pilleriappi10/ui/settings/settingsFragment.java
+++ app/src/main/java/fi/mobsit/pilleriappi10/ui/settings/settingsFragment.java
@@ -1,6 +1,5 @@
 package fi.mobsit.pilleriappi10.ui.settings;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import fi.mobsit.pilleriappi10.CreditsInfo;
 import fi.mobsit.pilleriappi10.databinding.FragmentSettingsBinding;
-import fi.mobsit.pilleriappi10.notifications.nofificationsManager;
 
 public class settingsFragment extends Fragment {
 
@@ -25,8 +22,6 @@ public class settingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-
         settingsViewModel =
                 new ViewModelProvider(this).get(settingsViewModel.class);
 
@@ -37,14 +32,7 @@ public class settingsFragment extends Fragment {
         settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-
                 buttonCredits.setText(s);
-                buttonCredits.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        openActivity2();
-                    }
-                });
             }
         });
 
@@ -59,22 +47,8 @@ public class settingsFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        nofificationsManager notification = new nofificationsManager(getActivity());
-        Boolean notificationPermission = notification.toBool();
-        notification.newNotification("medicineRemind", 1);
-    }
-
-    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    public void openActivity2() {
-        Intent intent = new Intent(this.getActivity(), CreditsInfo.class);
-        startActivity(intent);
-
     }
 }
