2cf8d3d4c366692edfd2cbbe944e7d719db778ee
Andreas <djdreviz@gmail.com>
2021-10-10 22:25:57 +0300

    Stringit jokaiseen tekstiin lisätty!

--- app/src/main/java/fi/mobsit/pilleriappi10/ui/settings/settingsFragment.java
+++ app/src/main/java/fi/mobsit/pilleriappi10/ui/settings/settingsFragment.java
@@ -1,6 +1,10 @@
 package fi.mobsit.pilleriappi10.ui.settings;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,17 +12,29 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.Locale;
+
 import fi.mobsit.pilleriappi10.CreditsInfo;
+import fi.mobsit.pilleriappi10.LocaleHelper;
+import fi.mobsit.pilleriappi10.MainActivity;
+import fi.mobsit.pilleriappi10.R;
 import fi.mobsit.pilleriappi10.databinding.FragmentSettingsBinding;
 import fi.mobsit.pilleriappi10.notifications.nofificationsManager;
 
 public class settingsFragment extends Fragment {
+
+    TextView language_dialog,helloworldtxt;
+    boolean lang_selected = true;
+    Context context;
+    Resources resources;
 
     private settingsViewModel settingsViewModel;
     private FragmentSettingsBinding binding;
@@ -53,6 +69,68 @@ public class settingsFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView2.setText(s);
+            }
+        });
+
+        final TextView language_dialog = binding.dialogLanguage;
+        settingsViewModel.getText2().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                language_dialog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final String[] language = {"ENGLISH", "SUOMI"};
+
+                        int checkedItem;
+
+                        if(lang_selected)
+                        {
+                            checkedItem = 0;
+                        }
+                        else
+                        {
+                            checkedItem=1;
+                        }
+
+                        //Oikeesti tässä pitäisi lukea mutta en saanut toimimaan:
+                        //final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+                        builder.setTitle("Select a language")
+                                .setSingleChoiceItems(language, checkedItem, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        language_dialog.setText(language[i]);
+                                        if(language[i].equals("ENGLISH"))
+                                        {
+                                            // oikeesti tässä pitäisi lukea mut en saa toimimaan:
+                                            // context = LocaleHelper.setLocale(MainActivity.this, "en");
+                                            context = LocaleHelper.setLocale(context, "en");
+                                            resources = context.getResources();
+
+                                            helloworldtxt.setText(resources.getString(R.string.language));
+                                        }
+                                        if(language[i].equals("SUOMI"))
+                                        {
+                                            context = LocaleHelper.setLocale(context, "fi");
+                                            resources = context.getResources();
+
+                                            helloworldtxt.setText(resources.getString(R.string.language));
+
+                                        }
+
+                                    }
+                                })
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                    }
+                                });
+                        builder.create().show();
+                    }
+                });
             }
         });
         return root;
