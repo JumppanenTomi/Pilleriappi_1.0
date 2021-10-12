ec42df6c8e47982e152146ead68f43b05ff59879
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2021-10-12 12:31:24 +0300

    Sovellus on valmis esitettäväksi

--- app/src/main/java/fi/mobsit/pilleriappi10/ui/home/HomeFragment.java
+++ app/src/main/java/fi/mobsit/pilleriappi10/ui/home/HomeFragment.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -51,7 +52,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView homeTextBox = view.findViewById(R.id.text_home);//initializing variable to get data from Textbox
+        TextView homeTextBox = view.findViewById(R.id.text_home);//initializing variable to get hjdata from Textbox
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);//initializing sharedpreferences to correct xml-file and setting it to private so other instances cant access it
         String jsonData = sharedPreferences.getString(MEDICINE_ARRAY, "");//getting old data from shared preferences file so it wont be wiped after creating new arraylist
 
@@ -61,18 +62,18 @@ public class HomeFragment extends Fragment {
         TextView textview = view.findViewById(R.id.text_home);
         TextView welcomeText = view.findViewById(R.id.home_textbox);
         json = jsonData;
-        textview.setText("Lääkkeesi");
+        textview.setText(getString(R.string.your_medicines));
         String welcomeMessage = getString(R.string.secondary_title_home);
         username =  welcomeMessage + username;
         welcomeText.setText(username);
         if (jsonData != null && jsonData.length() > 0) {
-            Log.v("MSG", "suoritettu");
             initlist();
             ListView listmed = view.findViewById(R.id.listmed);
             SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), lista, android.R.layout.simple_list_item_1, new String[]{"nimet"}, new int[]{android.R.id.text1});
             listmed.setAdapter(simpleAdapter);
         }else{
-
+            Toast.makeText(getActivity(), getString(R.string.list_medicine_empty), Toast.LENGTH_SHORT).show();
+            //Navigation.findNavController(view).navigate(R.id.navigation_home); //if this isnt users first time in application, then we lead him/her straight to home-fragment
         }
 
     }
@@ -87,7 +88,7 @@ public class HomeFragment extends Fragment {
                 String name = object.getString("medicineName");
                 String time = object.getString("time");
                 String date = object.getString("date");
-                String outPut = "Lääke: " + name + "\nKello: " + time + "\nPäivämäärä: " + date;
+                String outPut = getString(R.string.medicine) + ": " + name + "\n" + getString(R.string.time) + ": " + time + "\n" + getString(R.string.date) + ": " + date;
                 lista.add(createMed("nimet", outPut));
             }
 
